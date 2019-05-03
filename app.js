6cb7cddc8b8c17d711eb2e4c437177c6f4f1bba0
jonathanhovland <jonathan.hovland@yahoo.com>
2019-05-02 22:30:27 -0600

    The bar route is displaying

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-const express     = require('express')
-const bodyParser  = require('body-parser')
-const morgan      = require('morgan')
-const cors        = require('cors')
-const app         = module.exports = express()
-const port        = parseInt(process.env.PORT || 3007)
+const express = require ("express")
+const app = express()
+const bodyParser = require("body-parser")
+const morgan = require('morgan')
+const cors = require("cors")
+const port = process.env.PORT || 3000
 
 const barRoutes = require("./routes/bar")
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'))
-app.use(cors({origin: true, credentials: true})) // <= Disable if you don't need CORS
+app.use(cors())
 
 app.get("/", (req, res) => res.json({
-    "bar": "http://localhost:3007/bar",
+    "bar": "http://localhost:3000/bar",
 }));
 
 app.use("/bar", barRoutes)
 
 app.use(notFound)
+
 app.use(errorHandler)
 
-
-
 function notFound(req, res, next) {
-  res.status(404).send({error: 'Not found!', status: 404, url: req.originalUrl})
+    res.status(404).send({ error: 'Not found!', status: 404, url: req.originalUrl })
 }
 
-// eslint-disable-next-line
 function errorHandler(err, req, res, next) {
-  console.error('ERROR', err)
-  const stack =  process.env.NODE_ENV !== 'production' ? err.stack : undefined
-  res.status(500).send({error: err.message, stack, url: req.originalUrl})
+    console.error('ERROR', err)
+    const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
+    res.status(500).send({ error: err.message, stack, url: req.originalUrl })
 }
 
-app.listen(port, () => console.log("server is running on port 3007"))
+app.listen(port, () => console.log("server is running on port 3000"))
